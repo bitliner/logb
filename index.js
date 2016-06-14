44fd46ade36a51a3d457e8401febc2c9ef02df4d
bitliner <giovanni.gaglione@gmail.com>
2016-06-14 16:59:43 +0000

    +log-level based on env var

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ var bunyan = require('bunyan'),
 module.exports.getLogger = function(name, opts) {
     var matches, options;
 
+    var logLevel;
+
     opts = opts || {};
+
+    logLevel = process.env.LOG_LEVEL || 'info';
+
+
     matches = name.match(/\/[^\/]+$/g);
     if (matches) {
         name = matches[0];
@@ -19,7 +25,7 @@ module.exports.getLogger = function(name, opts) {
     options = {};
     options.name = name;
     options.stream = process.stdout;
-    options.level = opts.level || 'info';
+    options.level = logLevel;
 
 
     return bunyan.createLogger(options);
